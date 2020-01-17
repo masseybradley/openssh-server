736c858a6ac680b1f50c5d99bea4742f3be0657a
Bradley Massey <bradley.massey@yields.io>
2020-01-17 17:37:57 +0100

    wip: adding helm charts

--- journald.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import systemd.journal
-
-reader = systemd.journal.Reader()
-reader.add_match('CONTAINER_NAME=openssh')
-
-for msg in reader:
-    print('{CONTAINER_NAME}: {MESSAGE}'.format(**msg))
